dc938f87021c5db9df6028268db640f8bbfc66f4
Diego <diego_sanchez-brunete_apps1ma1819@cev.com>
2020-02-06 14:33:53 +0100

    jueves

--- bienestapp/login_register/RecoverViewController.swift
+++ bienestapp/login_register/RecoverViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class RecoverViewController: UIViewController {
     @IBOutlet weak var button: UIButton!
@@ -17,16 +18,49 @@ class RecoverViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    @IBAction func press(_ sender: Any) {
-        
-        
-        
         
     }
     
 
-}
+    @IBAction func press_recover(_ sender: UIButton) {
+    
+    
+        
+        if text_email.text!.isEmpty {
+        } else {
+        
+        let params: [String: String] = [
+            "email": text_email.text!
+        ]
+        
+        button.setTitle("Loading...", for: .normal )
+        
+        button.isEnabled = false
+
+    
+    
+            Alamofire.request("http://127.0.0.1:8888/Diego/bienestar/public/index.php/api/recover", method: .post, parameters: params).responseJSON { response in // method defaults to `.get`
+                
+                if response.result.value as! Int == 200 {
+                    self.text_error.isHidden = true
+
+                    self.button.setTitle("Check Email", for: .normal)
+                } else {
+                    
+                    self.text_error.isHidden = false
+                    self.button.setTitle("SEND NEW PASS", for: .normal)
+                    
+                }
+                
+                
+               
+            
+
+            
+            
+            
+
+            
+
+
+            }}}}
--- bienestapp/login_register/RegisterViewController.swift
+++ bienestapp/login_register/RegisterViewController.swift
@@ -7,24 +7,158 @@
 //
 
 import UIKit
+import Alamofire
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var name_text: UITextField!
+    @IBOutlet weak var email_text: UITextField!
+    @IBOutlet weak var password_text: UITextField!
+    
+    @IBOutlet weak var warning_label: UILabel!
+    @IBOutlet weak var button_send: UIButton!
+    
+    @IBOutlet weak var terms_switch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+ 
+    
+    
+    
+    @IBAction func press_button(_ sender: UIButton) {
+    
+        if  !self.email_text.text!.isEmpty {
+            
+            let params: [String: String] = [
+                "name": self.name_text.text!,
+                "email": self.email_text.text!,
+                "password": self.password_text.text!
+            ]
+                self.button_send.isEnabled = false
+                self.button_send.setTitle("Sending...", for: .normal )
+            
+        
+            Alamofire.request("http://127.0.0.1:8888/Diego/bienestar/public/index.php/api/users", method: .post, parameters: params).responseJSON { response in // method defaults to `.get`
+                
+                let json = response.result.value
+                    
+                    self.warning_label.isHidden = true
+                    
+                    let token: Token = Token(json: json as! [String : Any])
+                    
+                    saved_token = token.token
+                
+                    if token.token == "" {
+                        self.button_send.isEnabled = true
+                        self.warning_label.isHidden = false
+                        
+                    } else {
+                        self.getMainData()()
+                    }
+                    
+                    
+                
+                
+            }
+        
+        } else {
+            
+         self.warning_label.isHidden = false
+            
+            
+        }
+        
     }
-    */
+    
+    
+    func getMainData(){
+        Alamofire.request("http://127.0.0.1:8888/Diego/bienestar/public/index.php/api/myapps", method: .get, headers: ["token": saved_token]).responseJSON { response in // method defaults to `.get`
+            
+            if response.result.isFailure {
+                
+               print("esto no deberia pasar")
+                
+            } else {
+                
+                let date = Date()
+                let format = DateFormatter()
+                format.dateFormat = "yyyy-MM-dd"
+                let formattedDate = format.string(from: date)
+                
+                
+                let data = response.result.value as! [[String : Any]]
+                Alamofire.request("http://127.0.0.1:8888/Diego/bienestar/public/index.php/api/todayuse", method: .post, parameters: ["date":formattedDate], headers: ["token": saved_token]).responseJSON { response in // method defaults to `.get`
+                    
+                    let data_daily = response.result.value as! [[String : Any]]
+                    
+                    var daily_use: [String] = []
+                    
+                    
+                    for i in 0...(data.count-1) {
+                        
+                        var used_time: String = "0"
+                        
+                        if !data_daily.isEmpty {
+                            
+                            
+                            for o in 0...(data_daily.count-1) {
+                                
+                                if data[i]["id"] as! Int == data_daily[o]["app_id"] as! Int {
+                                    
+                                    var double_num: Double = data_daily[o]["used_time"] as! Double
+                                    
+                                    double_num /= 60000.00
+                                    
+                                    let int_num:Int = Int(double_num)
+                                    
+                                    let def_num: String = String(int_num)
+                                    
+                                    used_time = def_num
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                        
+                        daily_use.append(used_time)
+                        
+                    }
+                    
+                    
+                    cellsdatamain = MainViewData(todo: data, daily_usage: daily_use)
+                    
+                    Alamofire.request("http://127.0.0.1:8888/Diego/bienestar/public/index.php/api/restrictions", method: .get, headers: ["token": saved_token]).responseJSON { response in // method defaults to `.get`
+                        
+                        let restriction_data = response.result.value as! [[String : Any]]
+                        
+                        cellsdatarestrictions = RestrictionData(todo: restriction_data)
+                        
+                        self.dismiss(animated: true)
+                        
+                        self.performSegue(withIdentifier: "LoginToMain", sender: Any?.self)
+                        
+                    }
+                    
+                    
+                    
+                    
+                }
+                
+                
+            }
+            
+            
+            
+        }
+    }
+ 
 
 }
